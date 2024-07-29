3fde8f23826adb50f4052486f45506df1d46559b
HyeonWoo Park <gusdn5387@naver.com>
2024-07-29 19:28:07 +0900

    Initial Commit

---
+++ TCATutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCATutorial
+//
+//  Created by 박현우 on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by 박현우 on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
